823a6bf8a4c78de4aa1224885dffb8554c059524
Rafael Leonardo <rafamendo1994@gmail.com>
2023-01-17 18:17:13 +0100

    Haciendo js escalable

--- js/main.js
+++ js/main.js
@@ -1,30 +1,41 @@
-const img1 = document.querySelector(".img1");
-const descripcion1 = document.querySelector(".descripcion-proyecto1");
+// const img1 = document.querySelector(".img1");
+// const descripcion1 = document.querySelector(".descripcion-proyecto1");
 
-img1.addEventListener("mouseover", toggleInactivo)
-img1.addEventListener("mouseout", toggleInactivo)
+// img1.addEventListener("mouseover", toggleInactivo)
+// img1.addEventListener("mouseout", toggleInactivo)
+
+// function toggleInactivo(){
+//    descripcion1.classList.toggle("inactivo")
+// }
+
+// const img2 = document.querySelector(".img2");
+// const descripcion2 = document.querySelector(".descripcion-proyecto2");
+
+// img2.addEventListener("mouseover", toggleInactivo2)
+// img2.addEventListener("mouseout", toggleInactivo2)
+
+// function toggleInactivo2(){
+//    descripcion2.classList.toggle("inactivo")
+// }
+// const img3 = document.querySelector(".img3");
+// const descripcion3 = document.querySelector(".descripcion-proyecto3");
+
+// img3.addEventListener("mouseover", toggleInactivo3)
+// img3.addEventListener("mouseout", toggleInactivo3)
+
+// function toggleInactivo3(){
+//    descripcion3.classList.toggle("inactivo")
+// }
+
+
+const imgs = document.querySelectorAll(".img");
+const descripciones = document.querySelectorAll(".descripcion-proyecto");
+
+for (let i = 0; i < imgs.length; i++) {
+  imgs[i].addEventListener("mouseover", toggleInactivo);
+  imgs[i].addEventListener("mouseout", toggleInactivo);
+}
 
 function toggleInactivo(){
-   descripcion1.classList.toggle("inactivo")
+   this.nextElementSibling.classList.toggle("inactivo");
 }
-
-const img2 = document.querySelector(".img2");
-const descripcion2 = document.querySelector(".descripcion-proyecto2");
-
-img2.addEventListener("mouseover", toggleInactivo2)
-img2.addEventListener("mouseout", toggleInactivo2)
-
-function toggleInactivo2(){
-   descripcion2.classList.toggle("inactivo")
-}
-const img3 = document.querySelector(".img3");
-const descripcion3 = document.querySelector(".descripcion-proyecto3");
-
-img3.addEventListener("mouseover", toggleInactivo3)
-img3.addEventListener("mouseout", toggleInactivo3)
-
-function toggleInactivo3(){
-   descripcion3.classList.toggle("inactivo")
-}
-
-
