003c408e5f2168dbb08203e0639f0876e3bfa4ea
Rafael Leonardo <rafamendo1994@gmail.com>
2023-03-21 17:46:14 +0100

    Mas proyectos

--- js/main.js
+++ js/main.js
@@ -1,33 +1,3 @@
-// const img1 = document.querySelector(".img1");
-// const descripcion1 = document.querySelector(".descripcion-proyecto1");
-
-// img1.addEventListener("mouseover", toggleInactivo)
-// img1.addEventListener("mouseout", toggleInactivo)
-
-// function toggleInactivo(){
-//    descripcion1.classList.toggle("inactivo")
-// }
-
-// const img2 = document.querySelector(".img2");
-// const descripcion2 = document.querySelector(".descripcion-proyecto2");
-
-// img2.addEventListener("mouseover", toggleInactivo2)
-// img2.addEventListener("mouseout", toggleInactivo2)
-
-// function toggleInactivo2(){
-//    descripcion2.classList.toggle("inactivo")
-// }
-// const img3 = document.querySelector(".img3");
-// const descripcion3 = document.querySelector(".descripcion-proyecto3");
-
-// img3.addEventListener("mouseover", toggleInactivo3)
-// img3.addEventListener("mouseout", toggleInactivo3)
-
-// function toggleInactivo3(){
-//    descripcion3.classList.toggle("inactivo")
-// }
-
-
 const imgs = document.querySelectorAll(".img");
 const descripciones = document.querySelectorAll(".descripcion-proyecto");
 
@@ -36,6 +6,6 @@ for (let i = 0; i < imgs.length; i++) {
   imgs[i].addEventListener("mouseout", toggleInactivo);
 }
 
-function toggleInactivo(){
-   this.nextElementSibling.classList.toggle("inactivo");
+function toggleInactivo() {
+  this.nextElementSibling.classList.toggle("inactivo");
 }
