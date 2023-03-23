0d4df2e008d9b3091718f73b954363808845295a
Rafael Leonardo <rafamendo1994@gmail.com>
2023-03-23 18:45:09 +0100

    Edit Projects

--- js/main.js
+++ js/main.js
@@ -9,3 +9,17 @@ for (let i = 0; i < imgs.length; i++) {
 function toggleInactivo() {
   this.nextElementSibling.classList.toggle("inactivo");
 }
+const panels = document.querySelectorAll('.panel')
+
+panels.forEach(panel => {
+    panel.addEventListener('click', () => {
+        removeActiveClasses()
+        panel.classList.add('active')
+    })
+})
+
+function removeActiveClasses() {
+    panels.forEach(panel => {
+        panel.classList.remove('active')
+    })
+}
